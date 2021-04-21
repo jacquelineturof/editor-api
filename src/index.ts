6e3fab9f15bb2aaa8ca076c7e4466482820157c8
jacquelineturof <jacquelineturof@gmail.com>
2021-04-21 10:24:07 -0400

    update serve to return a promise

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express()
-    console.log(filename)
+
     app.listen(port, () => {
         console.log('Listening on port, ', port)
     })
