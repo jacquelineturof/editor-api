44c484c05a8289f42cad7f3ced490173010db89e
jacquelineturof <jacquelineturof@gmail.com>
2021-04-21 12:15:54 -0400

    setup proxy middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express()
+    
+    app.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }))
 
-    app.listen(port, () => {
-        console.log('Listening on port, ', port)
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject)
     })
 }
